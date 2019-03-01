fdde36f8cef7fdf302fb2d34b062056aefb45bf0
Adam Balsam <adam.balsam@acquia.com>
2019-03-01 14:16:06 -0500

    confirm

--- hooks/common/post-code-update/reinstall.sh
+++ hooks/common/post-code-update/reinstall.sh
@@ -8,5 +8,5 @@ drush --version
 /usr/local/bin/drush9  --version
 /usr/local/bin/drush9 @$site.$target_env list
 
-/usr/local/bin/drush9 @$site.$target_env tome:install
+/usr/local/bin/drush9 @$site.$target_env tome:install --yes
 /usr/local/bin/drush9 @$site.$target_env user:password admin admin
