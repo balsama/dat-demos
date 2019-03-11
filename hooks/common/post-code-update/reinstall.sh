0319b6f987012db04f6da5dd96604d123a05f869
Adam Balsam <adam.balsam@acquia.com>
2019-03-11 10:10:40 -0400

    Use aliases again

--- hooks/common/post-code-update/reinstall.sh
+++ hooks/common/post-code-update/reinstall.sh
@@ -3,5 +3,5 @@
 site="$1"
 target_env="$2"
 
-/usr/local/bin/drush9 @$site.$target_env tome:install --yes -vvv
+/usr/local/bin/drush9 @$site.$target_env tome:install @$site.$target_env --yes -vvv
 /usr/local/bin/drush9 @$site.$target_env user:password admin admin
