c90f34f89ed49dd7e0b31b57e8566c63dad9d5c1
Adam Balsam <adam.balsam@acquia.com>
2019-03-11 09:56:14 -0400

    Cleanup cloud hook instructions

--- hooks/common/post-code-deploy/reinstall.sh
+++ hooks/common/post-code-deploy/reinstall.sh
@@ -3,10 +3,5 @@
 site="$1"
 target_env="$2"
 
-which drush
-drush --version
-/usr/local/bin/drush9  --version
-/usr/local/bin/drush9 @$site.$target_env list
-
-/usr/local/bin/drush9 @$site.$target_env tome:install @$site.$target_env --yes
+/usr/local/bin/drush9 @$site.$target_env tome:install --yes
 /usr/local/bin/drush9 @$site.$target_env user:password admin admin
