18a99bec3d951359b9b332c0fa4671e735db0b5e
Adam Balsam <adam.balsam@acquia.com>
2019-03-21 07:44:15 -0400

    Remove reinstall on deploy

--- hooks/common/post-code-update/reinstall.sh
+++ hooks/common/post-code-update/reinstall.sh
@@ -3,6 +3,4 @@
 site="$1"
 target_env="$2"
 
-/usr/local/bin/drush9 @$site.$target_env drush site:install --existing-config --account-pass=admin --yes
-/usr/local/bin/drush9 @$site.$target_env pm:uninstall demo_umami_content
-/usr/local/bin/drush9 @$site.$target_env pm:enable demo_umami_content
+/usr/local/bin/drush9 @$site.$target_env status
