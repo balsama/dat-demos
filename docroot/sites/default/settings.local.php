f1c8d9b961bdeef4149be0fd560e995c5f403ceb
Adam Balsam <balsama@users.noreply.github.com>
2019-03-22 09:38:54 -0400

    Convert to config and sqlite instead of Tome (#3)

---
+++ docroot/sites/default/settings.local.php
@@ -0,0 +1,7 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'fixtures/sqlite/dev/.ht.sqlite',
+  'prefix' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite',
+  'driver' => 'sqlite',
+);
--- hooks/common/post-code-update/reinstall.sh
+++ hooks/common/post-code-update/reinstall.sh
@@ -3,5 +3,6 @@
 site="$1"
 target_env="$2"
 
-/usr/local/bin/drush9 @$site.$target_env tome:install @$site.$target_env --yes
-/usr/local/bin/drush9 @$site.$target_env user:password admin admin
+/usr/local/bin/drush9 @$site.$target_env site:install --existing-config --account-pass=admin --yes
+/usr/local/bin/drush9 @$site.$target_env pm:uninstall demo_umami_content
+/usr/local/bin/drush9 @$site.$target_env pm:enable demo_umami_content
