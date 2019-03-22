739ce46a4b4d3d18236a7d2a505b066724b663e4
Adam Balsam <adam.balsam@acquia.com>
2019-03-22 08:38:00 -0400

    Add cloud demploy config

--- hooks/common/post-code-update/reinstall.sh
+++ hooks/common/post-code-update/reinstall.sh
@@ -3,4 +3,6 @@
 site="$1"
 target_env="$2"
 
-/usr/local/bin/drush9 @$site.$target_env status
+/usr/local/bin/drush9 @$site.$target_env site:install --existing-config --account-pass=admin --yes
+/usr/local/bin/drush9 @$site.$target_env pm:uninstall demo_umami_content
+/usr/local/bin/drush9 @$site.$target_env pm:enable demo_umami_content
