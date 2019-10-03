e1ca5ad593fd340eb4732dd23e73d9d718cbc5d6
Adam Balsam <adam.balsam@acquia.com>
2019-10-03 16:37:03 -0400

    rm local setting

--- docroot/sites/default/settings.local.php
+++
@@ -1,7 +0,0 @@
-<?php
-$databases['default']['default'] = array (
-  'database' => 'fixtures/sqlite/dev/.ht.sqlite',
-  'prefix' => '',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite',
-  'driver' => 'sqlite',
-);
